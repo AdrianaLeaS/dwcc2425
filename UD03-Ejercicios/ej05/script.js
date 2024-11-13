773733465364115282ce70bb388a88b013bdf0db
AdrianaLeaS <a22adrianals@iessanclemente.net>
2024-11-13 17:26:00 +0100

    definitivo

---
+++ UD03-Ejercicios/ej05/script.js
@@ -0,0 +1,24 @@
+
+ // TODO: O alumnado debe usar o obxecto navigator para obter e
+mostrar na páxina:
+ // 1. navigator.appName
+ // 2. navigator.appVersion
+ // 3. navigator.language
+ // 4. navigator.platform
+ // TODO: O alumnado debe usar o obxecto screen para obter e mostrar
+na páxina:
+ // 1. screen.width
+ // 2. screen.height
+ // TODO: O alumnado debe implementar as funcións para o obxecto
+location:
+ // - actualizarPaxina: Usar location.reload() para recargar a
+páxina actual.
+ // - irAGoogle: Usar location.assign("https://www.google.com") para
+ir a Google.
+
+ // TODO: O alumnado debe implementar as funcións para o obxecto
+history: (fixate no html como se chaman)
+ // - irAtras: Usar history.back() para ir á páxina anterior no
+historial.
+ // - irAdiante: Usar history.forward() para ir á páxina seguinte no
+historial.
