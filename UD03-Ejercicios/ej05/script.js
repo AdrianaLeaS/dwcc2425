ef0bd06bbcf8f3d58ea4c94a215437855128d06c
AdrianaLeaS <a22adrianals@iessanclemente.net>
2024-11-13 17:34:47 +0100

    definitivisima

--- UD03-Ejercicios/ej05/script.js
+++ UD03-Ejercicios/ej05/script.js
@@ -1,24 +1,27 @@
 
  // TODO: O alumnado debe usar o obxecto navigator para obter e
-mostrar na páxina:
- // 1. navigator.appName
- // 2. navigator.appVersion
- // 3. navigator.language
- // 4. navigator.platform
+// mostrar na páxina:
+
+document.getElementById("appName").textContent = navigator.appName;
+document.getElementById("appVersion").textContent = navigator.appVersion;
+document.getElementById("language").textContent = navigator.language;
+document.getElementById("platform").textContent = navigator.platform;
+
  // TODO: O alumnado debe usar o obxecto screen para obter e mostrar
-na páxina:
- // 1. screen.width
- // 2. screen.height
- // TODO: O alumnado debe implementar as funcións para o obxecto
-location:
- // - actualizarPaxina: Usar location.reload() para recargar a
-páxina actual.
- // - irAGoogle: Usar location.assign("https://www.google.com") para
-ir a Google.
+//na páxina:
+document.getElementById("screenWidth").textContent = screen.width;
+document.getElementById("screenHeight").textContent = screen.height;
 
  // TODO: O alumnado debe implementar as funcións para o obxecto
-history: (fixate no html como se chaman)
+//location:
+ // - actualizarPaxina: Usar location.reload() para recargar a
+//páxina actual.
+ // - irAGoogle: Usar location.assign("https://www.google.com") para
+//ir a Google.
+
+ // TODO: O alumnado debe implementar as funcións para o obxecto
+//history: (fixate no html como se chaman)
  // - irAtras: Usar history.back() para ir á páxina anterior no
-historial.
+//historial.
  // - irAdiante: Usar history.forward() para ir á páxina seguinte no
-historial.
+//historial.
