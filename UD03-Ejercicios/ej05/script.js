e94a1831b843eb8556b3aea4aadf308ef4ec10f1
AdrianaLeaS <a22adrianals@iessanclemente.net>
2024-11-17 21:49:11 +0100

    todo 171124

--- UD03-Ejercicios/ej05/script.js
+++ UD03-Ejercicios/ej05/script.js
@@ -1,24 +1,33 @@
 
  // TODO: O alumnado debe usar o obxecto navigator para obter e
-mostrar na páxina:
- // 1. navigator.appName
- // 2. navigator.appVersion
- // 3. navigator.language
- // 4. navigator.platform
+// mostrar na páxina:
+document.getElementById("appName").textContent = navigator.appName;
+document.getElementById("appVersion").textContent = navigator.appVersion;
+document.getElementById("language").textContent = navigator.language;
+document.getElementById("platform").textContent = navigator.platform;
+
  // TODO: O alumnado debe usar o obxecto screen para obter e mostrar
-na páxina:
- // 1. screen.width
- // 2. screen.height
- // TODO: O alumnado debe implementar as funcións para o obxecto
-location:
- // - actualizarPaxina: Usar location.reload() para recargar a
-páxina actual.
- // - irAGoogle: Usar location.assign("https://www.google.com") para
-ir a Google.
+//na páxina:
+document.getElementById("screenWidth").textContent = screen.width;
+document.getElementById("screenHeight").textContent = screen.height;
 
  // TODO: O alumnado debe implementar as funcións para o obxecto
-history: (fixate no html como se chaman)
- // - irAtras: Usar history.back() para ir á páxina anterior no
-historial.
- // - irAdiante: Usar history.forward() para ir á páxina seguinte no
-historial.
+//location:
+function actualizarPaxina () {
+    window.location.reload();
+}
+
+function irAGoogle () {
+    window.location.assign ("https://www.google.com");
+}
+ 
+ // TODO: O alumnado debe implementar as funcións para o obxecto
+//history: (fixate no html como se chaman)
+function irAtras () {
+    window.history.back;
+}
+
+function irAdiante() {
+    window.history.forward;
+}
+
