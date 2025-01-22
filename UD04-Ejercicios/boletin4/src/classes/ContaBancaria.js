948ab5b1a9a4f1efec30ba3de50b29ee58f74ae0
AdrianaLeaS <a22adrianals@iessanclemente.net>
2025-01-22 14:42:50 +0100

    220125b

---
+++ UD04-Ejercicios/boletin4/src/classes/ContaBancaria.js
@@ -0,0 +1,10 @@
+export class ContaBancaria {
+    _titular;
+    _saldoInicial;
+
+    constructor (titular, saldoInicial) {
+        this._titular = titular;
+        this._saldoInicial = saldoInicial;
+    }
+    
+}
